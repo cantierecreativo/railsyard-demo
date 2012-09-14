e35510a2e1e1e3a2697534bc5bf620feec18d180
nebirhos <nebirhos@aol.com>
2012-09-14 10:30:04 +0200

    Brand new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YardRecords::Application.config.secret_token = 'cd7c66b5cc52f316d9f8c5fb3d4b8ed37ae4b0e3ad7f3340363ed54875fa87be7b33a10dec512d711dd06d750c5df2b75f86903d46a8484a4681de199b427a3a'
