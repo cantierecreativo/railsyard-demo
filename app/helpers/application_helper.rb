63c10c37e98a37fb21613c5cf0dacf39c094a6fe
nebirhos <nebirhos@aol.com>
2012-09-17 05:58:38 +0200

    PrettyUrl for artists#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def artists_links(artists)
     if artists.length == 1
       a = artists.first
-      link_to a.name, artist_path(a)
+      link_to a.name, show_artists_path(pretty_url: a.pretty_url)
     else
       link_to "Various artists", '#'
     end
