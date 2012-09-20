e6b4c592b5bb5f4d8b73f9765e3e9a086bbd9428
nebirhos <nebirhos@aol.com>
2012-09-20 18:18:00 +0200

    Moar example images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed lorem in mauris condimentum eleifend. Proin vitae enim vel orci semper auctor sed sit amet ipsum. Maecenas sapien lacus, vestibulum quis egestas eget, molestie tristique turpis. Quisque sodales imperdiet enim ac tempus. Donec venenatis sodales tortor, sed bibendum arcu scelerisque in. Duis vulputate feugiat ullamcorper. Quisque molestie libero et mauris rutrum scelerisque. Donec scelerisque tincidunt ullamcorper. Nam cursus tincidunt nisi quis gravida. Donec aliquam lacus ac enim ultricies non pulvinar massa luctus. Sed imperdiet ante quis tortor porttitor quis rutrum tellus pretium. Aenean euismod imperdiet aliquet. Phasellus at neque a eros bibendum viverra quis vel orci. Maecenas et augue erat, sit amet porttitor nibh."
+open_image = lambda {|name| File.read(File.expand_path(File.join __FILE__, "../../app/assets/images/soundrock/#{name}")) }
 
 ### Users and settings ###
 User.create! name: "Yard Admin", email: "admin@example.com", password: "changeme"
@@ -33,7 +34,24 @@ end
 ### Artists ###
 antani = Artist.create(name: "The Antanis", country: "Italy", biography: lorem, homepage_url: "http://antani.co.uk/")
 iggy = Artist.create(name: "Iggy Pop", country: "United States", biography: lorem, homepage_url: "http://www.iggypop.com/")
-silvia = Artist.create(name: "Greedy Silvia And The Kinky", country: "United Kingdom")
+silvia = Artist.create(name: "Greedy Silvia And The Kinky", country: "United Kingdom", biography: lorem, homepage_url: "http://nyan.cat/")
+
+### Artists photos ###
+p = antani.photos.new
+p.image = open_image["banner3.jpg"]
+p.save!
+
+p = iggy.photos.new
+p.image = open_image["banner2.jpg"]
+p.save!
+
+p = silvia.photos.new
+p.image = open_image["img22.jpg"]
+p.save!
+p = silvia.photos.new
+p.image = open_image["banner4.jpg"]
+p.save!
+
 
 ### Release Types ###
 lp = ReleaseType.create name: "LPs"
@@ -62,10 +80,8 @@ r1.tracklist = <<HTML
 HTML
 r1.release_date = Date.today
 r1.published = true
-a1 = Railsyard::Gallery::Image.create
-a1.imageable_relation = "artwork"
-a1.imageable = r1
-a1.image = File.read(File.expand_path(File.join __FILE__, "../../files/img16.jpg"))
+a1 = r1.build_artwork
+a1.image = open_image["img25.jpg"]
 a1.save!
 r1.save!
 
@@ -89,9 +105,7 @@ r2.tracklist = <<HTML
 HTML
 r2.release_date = Date.today
 r2.published = true
-a2 = Railsyard::Gallery::Image.create
-a2.imageable_relation = "artwork"
-a2.imageable = r2
-a2.image = File.read(File.expand_path(File.join __FILE__, "../../files/img25.jpg"))
+a2 = r2.build_artwork
+a2.image = open_image["img16.jpg"]
 a2.save!
 r2.save!
